0756be760a9d0e5ff4ac25d8e8477186d1ecb64e
Taras Romanyk <rredcat@gmail.com>
2017-09-09 11:16:02 +0300

    base chat

---
+++ server/index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/client', function(req, res){
+  res.sendFile(__dirname + '/client.html');
+});
+
+
+io.on('connection', function(socket){
+  console.log('connection')
+  socket.on('message', function(msg){
+    console.log(msg);
+    io.emit('message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});

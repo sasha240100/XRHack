d2da66d7e6891bf7066c777c3dadf0561af87906
Alexander Buzin <alexbuzin88@gmail.com>
2017-09-09 14:31:36 +0300

    Fix quaternion

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,39 +3,48 @@ import GyroNorm from 'gyronorm';
 
 import io from 'socket.io-client';
 
-const gyro = new GyroNorm();
+// const gyro = new GyroNorm();
 const socket = io(`${window.NET_IP || '192.168.1.19'}:3000`);
 
+const promise = new FULLTILT.getDeviceOrientation({ 'type': 'world' });
+
+promise.then(controller => {
+  // Store the returned FULLTILT.DeviceOrientation object
+  controller.start(data => {
+    const quat = controller.getFixedFrameQuaternion();
+    socket.emit('data', [quat.x, quat.y, quat.z, quat.w]);
+  })
+});
 // console.log(socket);
 
 // socket.emit('message', 'hello');
 
 // debugger;
-
-gyro.init().then(() => {
-  gyro.start(data => {
-    console.log(data);
-    socket.emit('data', [data.do.alpha, data.do.beta, data.do.gamma]);
-
-    // Process:
-    // data.do.alpha	( deviceorientation event alpha value )
-    // data.do.beta		( deviceorientation event beta value )
-    // data.do.gamma	( deviceorientation event gamma value )
-    // data.do.absolute	( deviceorientation event absolute value )
-
-    // data.dm.x		( devicemotion event acceleration x value )
-    // data.dm.y		( devicemotion event acceleration y value )
-    // data.dm.z		( devicemotion event acceleration z value )
-
-    // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
-    // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
-    // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
-
-    // data.dm.alpha	( devicemotion event rotationRate alpha value )
-    // data.dm.beta		( devicemotion event rotationRate beta value )
-    // data.dm.gamma	( devicemotion event rotationRate gamma value )
-  });
-}).catch(e => {
-  alert(e)
-  // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-});
+//
+// gyro.init().then(() => {
+//   gyro.start(data => {
+//     console.log(data);
+//     socket.emit('data', [data.do.alpha, data.do.beta, data.do.gamma]);
+//
+//     // Process:
+//     // data.do.alpha	( deviceorientation event alpha value )
+//     // data.do.beta		( deviceorientation event beta value )
+//     // data.do.gamma	( deviceorientation event gamma value )
+//     // data.do.absolute	( deviceorientation event absolute value )
+//
+//     // data.dm.x		( devicemotion event acceleration x value )
+//     // data.dm.y		( devicemotion event acceleration y value )
+//     // data.dm.z		( devicemotion event acceleration z value )
+//
+//     // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
+//     // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
+//     // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
+//
+//     // data.dm.alpha	( devicemotion event rotationRate alpha value )
+//     // data.dm.beta		( devicemotion event rotationRate beta value )
+//     // data.dm.gamma	( devicemotion event rotationRate gamma value )
+//   });
+// }).catch(e => {
+//   alert(e)
+//   // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
+// });
--- src/visual/elements/controller.js
+++ src/visual/elements/controller.js
@@ -1,5 +1,5 @@
 import {Importer, MeshComponent, Group} from 'whs';
-import {MeshBasicMaterial, DoubleSide, Color} from 'three';
+import {MeshBasicMaterial, DoubleSide, Color, AxisHelper} from 'three';
 import io from 'socket.io-client';
 
 const socket = io(`${window.NET_IP || '192.168.1.19'}:3000`);
@@ -11,7 +11,13 @@ import MTLModule from '../modules/MTLModule';
 
 export const placementGroup = new Group();
 placementGroup.position.set(10, 5, -5);
+// placementGroup.rotation.set(Math.PI / 2, 0, Math.PI / 2);
 placementGroup.addTo(app);
+
+const fixGroup = new Group();
+// fixGroup.rotation.set(0, 0, 0);
+fixGroup.native.add(new AxisHelper());
+fixGroup.addTo(placementGroup);
 
 const scale = 0.01;
 
@@ -36,16 +42,17 @@ new Importer({
     })
   ],
 
-  scale: [scale, scale, scale]
-}).addTo(placementGroup).then(controller => {
+  scale: [scale, scale, scale],
+  rotation: [0, -Math.PI / 2, -Math.PI / 2]
+}).addTo(fixGroup).then(controller => {
   const degToRad = (deg) => deg / 180 * Math.PI;
 
   socket.on('update', data => {
-
-    controller.rotation.set(
-      degToRad(data[0]),
-      degToRad(data[1]),
-      degToRad(data[2])
+    fixGroup.quaternion.set(
+      data[0],
+      data[2],
+      -data[1],
+      data[3]
     );
   })
 });;
